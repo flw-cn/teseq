7a954fb7a80293683680d6010dcf96393813dab0
Micah Cowan <micah@cowan.name>
2008-06-27 17:45:58 -0700

    Start of implementation (and basic test) for ring buffer.

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
+CHECKMK = checkmk
 ESEQ_SOURCES = src/eseq.c
 
 all: eseq
@@ -5,5 +6,20 @@ all: eseq
 eseq: $(ESEQ_SOURCES)
 	$(CC) $(CPPFLAGS) $(CFLAGS) $(ESEQ_SOURCES) -o $@
 
-check: eseq
+check: unit-tests functionality-tests
+
+.PHONY: unit-tests
+unit-tests: src/test-buffer
+	src/test-buffer
+
+src/test-buffer: src/ringbuf.h src/ringbuf.c src/test-ringbuf.c
+	$(CC) $(CPPFLAGS) $(CFLAGS) src/ringbuf.c src/test-ringbuf.c -lcheck -o $@
+
+.SUFFIXES: .cm
+
+.cm.c:
+	$(CHECKMK) $< > $@
+
+.PHONY: functionality-tests
+functionality-tests: eseq
 	cd tests && ./run
---
+++ src/ringbuf.h
@@ -0,0 +1,36 @@
+/* buffer.h: input read-ahead ring buffer implementation. */
+
+/*
+   Copyright (C) 2008 Micah Cowan
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions
+   are met:
+
+   1. Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+   2. Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in
+      the documentation and/or other materials provided with the
+      distribution.
+
+   THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+   IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+   WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+   ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR
+   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+   OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+struct ringbuf;
+
+struct ringbuf *ringbuf_new (void);
+void ringbuf_put (struct ringbuf *, unsigned char);
+int ringbuf_get (struct ringbuf *);
+
+/* vim:set sts=8 ts=8 sw=8 noet: */
