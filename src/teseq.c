f7a44dc2082beb1aa4357336319114d136a316c6
Micah Cowan <micah@addictivecode.org>
2015-08-13 13:30:40 -0700

    Die on control characters in output, too.

--- src/teseq.c
+++ src/teseq.c
@@ -157,7 +157,7 @@ delay_read (FILE *f, struct delay *d)
 void
 print_esc_char (struct processor *p, unsigned char c)
 {
-    assert(c > 0 && c < 0x7f);
+    assert(c >= 0x20 && c < 0x7f);
 
     if (c == C_ESC)
       putter_puts (p->putr, " Esc");
